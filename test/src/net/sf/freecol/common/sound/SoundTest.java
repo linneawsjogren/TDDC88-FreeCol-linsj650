4acd304c9daf84d3b4e4493f4d5f68c039ebbfd6
linneawsjogren <linnea.l.sjogren@gmail.com>
2018-10-04 10:59:54 +0200

    step 7 lab 4 test3

--- test/src/net/sf/freecol/common/sound/SoundTest.java
+++ test/src/net/sf/freecol/common/sound/SoundTest.java
@@ -65,7 +65,7 @@ public class SoundTest extends FreeColTestCase {
 
     private void playSound(String id) {
         File file = ResourceManager.getAudio(id);
-        assertNotNull("No sound resource: " + id, file);
+        assertNotNull("No sound resource here: " + id, file);
         try {
             soundPlayer.playOnce(file);
             try { // Just play the beginning of the sound to check it works
